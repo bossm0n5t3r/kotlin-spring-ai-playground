5e7d8386bb3316555a5b77af83c01c4541b6a829
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-09-22 12:00:00 +0900

    :tada:

---
+++ src/test/kotlin/me/bossm0n5t3r/kotlinspringaiplayground/KotlinSpringAiPlaygroundApplicationTests.kt
@@ -0,0 +1,11 @@
+package me.bossm0n5t3r.kotlinspringaiplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringAiPlaygroundApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}

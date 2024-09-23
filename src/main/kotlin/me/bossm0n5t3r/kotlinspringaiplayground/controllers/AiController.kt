bd161e443d4cab236964447b5b818f78a11692cb
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-09-24 03:30:00 +0900

    feat: Add /chat-client-responses API

--- src/main/kotlin/me/bossm0n5t3r/kotlinspringaiplayground/controllers/AiController.kt
+++ src/main/kotlin/me/bossm0n5t3r/kotlinspringaiplayground/controllers/AiController.kt
@@ -1,9 +1,13 @@
 package me.bossm0n5t3r.kotlinspringaiplayground.controllers
 
 import me.bossm0n5t3r.kotlinspringaiplayground.dtos.Answer
+import me.bossm0n5t3r.kotlinspringaiplayground.dtos.MathematicianPublications
 import me.bossm0n5t3r.kotlinspringaiplayground.dtos.Question
 import me.bossm0n5t3r.kotlinspringaiplayground.dtos.toAnswer
 import me.bossm0n5t3r.kotlinspringaiplayground.services.BasicAiService
+import me.bossm0n5t3r.kotlinspringaiplayground.services.ChatClientResponsesService
+import org.springframework.ai.chat.model.ChatResponse
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class AiController(
     private val basicAiService: BasicAiService,
+    private val chatClientResponsesService: ChatClientResponsesService,
 ) {
     @PostMapping("/generation/autoconfigured")
     fun generationWithAutoconfigured(
@@ -21,4 +26,16 @@ class AiController(
     fun generationWithProgrammatically(
         @RequestBody question: Question,
     ): Answer = basicAiService.generationWithProgrammatically(question.text).toAnswer()
+
+    @PostMapping("/chat-client-responses/chat-response")
+    fun returningAChatResponse(
+        @RequestBody question: Question,
+    ): ChatResponse = chatClientResponsesService.returningAChatResponse(question.text)
+
+    @GetMapping("/chat-client-responses/entity")
+    fun returningAnEntity(): MathematicianPublications = chatClientResponsesService.returningAnEntity()
+
+    @GetMapping("/chat-client-responses/entity-with-parameterized-type-reference")
+    fun returningAnEntityWithParameterizedTypeReference(): List<MathematicianPublications> =
+        chatClientResponsesService.returningAnEntityWithParameterizedTypeReference()
 }

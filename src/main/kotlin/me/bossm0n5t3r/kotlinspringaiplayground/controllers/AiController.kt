50ed95b0bfb8a38a51678a6b9d0f2433327341fb
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-09-26 03:39:00 +0900

    feat: Update /chat-client-responses API

--- src/main/kotlin/me/bossm0n5t3r/kotlinspringaiplayground/controllers/AiController.kt
+++ src/main/kotlin/me/bossm0n5t3r/kotlinspringaiplayground/controllers/AiController.kt
@@ -7,10 +7,12 @@ import me.bossm0n5t3r.kotlinspringaiplayground.dtos.toAnswer
 import me.bossm0n5t3r.kotlinspringaiplayground.services.BasicAiService
 import me.bossm0n5t3r.kotlinspringaiplayground.services.ChatClientResponsesService
 import org.springframework.ai.chat.model.ChatResponse
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
 
 @RestController
 class AiController(
@@ -38,4 +40,12 @@ class AiController(
     @GetMapping("/chat-client-responses/entity-with-parameterized-type-reference")
     fun returningAnEntityWithParameterizedTypeReference(): List<MathematicianPublications> =
         chatClientResponsesService.returningAnEntityWithParameterizedTypeReference()
+
+    @PostMapping("/chat-client-responses/streaming-responses", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun streamingResponses(
+        @RequestBody question: Question,
+    ): Flux<String> = chatClientResponsesService.streamingResponses(question.text)
+
+    @GetMapping("/chat-client-responses/streaming-responses-with-converter")
+    fun streamingResponsesWithConverter(): List<MathematicianPublications> = chatClientResponsesService.streamingResponsesWithConverter()
 }
